9acd2fab9d6f114941a3c1e87b4a6d5961814989
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-12-26 22:31:36 +0900

    feat: next-themes 이용한 dark/light 모드 구현
    - next-themes 라이브러리 추가
    - ThemeProvider로 전체 테마 상태 관리
    - ThemeBtn 컴포넌트를 통해 다크/라이트 모드 전환 기능 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        dark: "#121619",
+        light: "#f9f9f9",
       },
     },
   },
+  darkMode: ["class"],
   plugins: [],
-} satisfies Config;
+};
+export default config;
