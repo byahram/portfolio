be96e822459c07fc2f3e935705774372b5db1445
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-01-19 22:07:40 +0900

    feat: 타이틀 수정 및 불필요한 텍스트 삭제

--- src/app/voice/page.tsx
+++ src/app/voice/page.tsx
@@ -3,15 +3,10 @@ import Giscus from "@/components/Giscus";
 export default function Voice() {
   return (
     <section id="voice" className="my-10 md:my-16">
-      <h1 className="mb-8 text-2xl font-medium tracking-tighter text-center">
-        Voice
+      <h1 className="mb-8 text-2xl font-medium tracking-tighter">
+        Share Your Thoughts 🙌
       </h1>
-      <div className="text-center mb-8">
-        <p className="text-lg font-semibold text-gray-700">
-          Let your voice be heard.
-        </p>
-        <p className="text-sm text-gray-500">당신의 의견을 들려주세요.</p>
-      </div>
+
       <div id="giscus_wrap" className="max-w-4xl mx-auto">
         <Giscus />
       </div>
