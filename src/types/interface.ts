9ce13fd14f9ed860fc702df5dd76dda2fbdfc404
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-01-24 14:03:29 +0900

    feat: Career Notion 데이터 타입 정의 추가

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -12,3 +12,39 @@ export interface ProjectData {
   description: string;
   tech: string[];
 }
+// [Career] Notion 원시 데이터 구조
+export interface NotionCareerProps {
+  careerId: { title: { plain_text: string }[] };
+  projectId: { relation: { id: string }[] };
+  company: { rich_text: { plain_text: string }[] };
+  description: { rich_text: { plain_text: string }[] };
+  team: { rich_text: { plain_text: string }[] };
+  position: { rich_text: { plain_text: string }[] };
+  role: { rich_text: { plain_text: string }[] };
+  rnr: { multi_select: { name: string }[] };
+  techs: { multi_select: { name: string }[] };
+  employment_period: {
+    date: {
+      start: string;
+      end: string | null;
+    } | null;
+  };
+  tenure: { rich_text: { plain_text: string }[] };
+  status: { status: { name: string } };
+}
+// [Career] 가공된 데이터
+export interface CareerData {
+  careerId: string;
+  projectId: string[];
+  company: string;
+  description: string;
+  team: string;
+  position: string;
+  role: string;
+  rnr: string[];
+  techs: string[];
+  employmentFrom: string | null;
+  employmentTo: string | null;
+  tenure: string;
+  status: string;
+}
