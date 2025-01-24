1342784b701ad3fdc2781e7bdeaa2f7985154c5a
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-01-24 15:54:54 +0900

    add: Career 관련 타입 정의 및 데이터 처리 함수 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,75 +1,32 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Certification from "@/components/about/Certification";
 import Education from "@/components/about/Education";
 import Profile from "@/components/about/Profile";
 import Skills from "@/components/about/Skills";
 import Career from "@/components/about/Career";
 import Line from "@/components/Line";
+import ErrorMessage from "@/components/common/ErrorMessage";
+import { CareerData } from "@/types/career";
+import { fetchCareerData } from "@/lib/apiList";
 import {
   certificationList,
   educationList,
   profile,
   skillsList,
 } from "@/store/store";
-import { CareerData, NotionCareerProps } from "@/types/interface";
-import { useEffect, useState } from "react";
-import ErrorMessage from "@/components/common/ErrorMessage";
-
-const processCareerData = (rawData: NotionCareerProps[]): CareerData[] => {
-  return rawData.map((item) => {
-    const {
-      careerId,
-      projectId,
-      company,
-      description,
-      team,
-      position,
-      role,
-      rnr,
-      techs,
-      employment_period,
-      tenure,
-      status,
-    } = item;
-
-    return {
-      careerId: careerId.title.map((text) => text.plain_text).join(""),
-      projectId: projectId.relation.map((rel) => rel.id),
-      company: company.rich_text.map((text) => text.plain_text).join(""),
-      description: description.rich_text
-        .map((text) => text.plain_text)
-        .join(""),
-      team: team.rich_text.map((text) => text.plain_text).join(""),
-      position: position.rich_text.map((text) => text.plain_text).join(""),
-      role: role.rich_text.map((text) => text.plain_text).join(""),
-      rnr: rnr.multi_select.map((item) => item.name),
-      techs: techs.multi_select.map((techItem) => techItem.name),
-      employmentFrom: employment_period?.date?.start || null,
-      employmentTo: employment_period?.date?.end || null,
-      tenure: tenure.rich_text.map((text) => text.plain_text).join(""),
-      status: status.status.name,
-    };
-  });
-};
 
 export default function About() {
-  const [experience, setExperience] = useState<CareerData[]>([]);
+  const [career, setCareer] = useState<CareerData[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  const fetchCareerData = async () => {
+  const fetchCareer = async () => {
     try {
-      const response = await fetch("/api/career", { method: "GET" });
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data: { properties: NotionCareerProps }[] = await response.json();
-      const propertiesOnly = data.map((item) => item.properties);
-      const processedData = processCareerData(propertiesOnly);
-      console.log(" processedData :: ", processedData);
-      setExperience(processedData);
+      const processedData = await fetchCareerData();
+      console.log("fetchCareerData processedData ---> ", processedData);
+      setCareer(processedData);
     } catch (error) {
       console.error("Error fetching data:", error);
       setError("Failed to fetch data. Please try again later.");
@@ -79,7 +36,7 @@ export default function About() {
   };
 
   useEffect(() => {
-    fetchCareerData();
+    fetchCareer();
   }, []);
 
   if (error) {
@@ -95,7 +52,7 @@ export default function About() {
       <Line />
       <Education data={educationList} isLoading={isLoading} />
       <Line />
-      <Career data={experience} isLoading={isLoading} />
+      <Career data={career} isLoading={isLoading} />
       <Line />
       <Skills data={skillsList} isLoading={isLoading} />
       <Line />
---
+++ src/types/career.ts
@@ -0,0 +1,76 @@
+// [Career] Notion 원시 데이터 구조
+export interface NotionCareerProps {
+  careerId: { title: { plain_text: string }[] };
+  projectId: { relation: { id: string }[] };
+  company: { rich_text: { plain_text: string }[] };
+  description: { rich_text: { plain_text: string }[] };
+  team: { rich_text: { plain_text: string }[] };
+  position: { rich_text: { plain_text: string }[] };
+  role: { rich_text: { plain_text: string }[] };
+  rnr: { multi_select: { name: string }[] };
+  techs: { multi_select: { name: string }[] };
+  employment_period: {
+    date: {
+      start: string;
+      end: string | null;
+    } | null;
+  };
+  tenure: { rich_text: { plain_text: string }[] };
+  status: { status: { name: string } };
+}
+// [Career] 가공된 데이터
+export interface CareerData {
+  careerId: string;
+  projectId: string[];
+  company: string;
+  description: string;
+  team: string;
+  position: string;
+  role: string;
+  rnr: string[];
+  techs: string[];
+  employmentFrom: string | null;
+  employmentTo: string | null;
+  tenure: string;
+  status: string;
+}
+
+// [Career] 데이터 처리 함수
+export const processCareerData = (
+  rawData: NotionCareerProps[]
+): CareerData[] => {
+  return rawData.map((item) => {
+    const {
+      careerId,
+      projectId,
+      company,
+      description,
+      team,
+      position,
+      role,
+      rnr,
+      techs,
+      employment_period,
+      tenure,
+      status,
+    } = item;
+
+    return {
+      careerId: careerId.title.map((text) => text.plain_text).join(""),
+      projectId: projectId.relation.map((rel) => rel.id),
+      company: company.rich_text.map((text) => text.plain_text).join(""),
+      description: description.rich_text
+        .map((text) => text.plain_text)
+        .join(""),
+      team: team.rich_text.map((text) => text.plain_text).join(""),
+      position: position.rich_text.map((text) => text.plain_text).join(""),
+      role: role.rich_text.map((text) => text.plain_text).join(""),
+      rnr: rnr.multi_select.map((item) => item.name),
+      techs: techs.multi_select.map((techItem) => techItem.name),
+      employmentFrom: employment_period?.date?.start || null,
+      employmentTo: employment_period?.date?.end || null,
+      tenure: tenure.rich_text.map((text) => text.plain_text).join(""),
+      status: status.status.name,
+    };
+  });
+};
