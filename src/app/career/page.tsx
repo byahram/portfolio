863cfc2309bb3be7c220077e5a938b651c21aa7c
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-02-02 22:38:29 +0900

    feat: CareerSkeleton 추가 및 Introduction 컴포넌트 분리

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import BadgeText from "@/components/BadgeText";
 import Line from "@/components/Line";
 import { ApiResponse, CareerProjectData } from "@/types/career";
 import { fetchCareerData, fetchCareerProjectData } from "@/lib/apiList";
 import ErrorMessage from "@/components/common/ErrorMessage";
+import CareerSkeleton from "@/components/career/CareerSkeleton";
+import Introduction from "@/components/career/Introduction";
 
 export default function Experience() {
   const [careers, setCareers] = useState<ApiResponse[]>([]);
@@ -63,76 +64,72 @@ export default function Experience() {
     return <ErrorMessage message={error} />;
   }
 
-  if (isLoadingCareer || isLoadingProject) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <section id="work" className="my-10 md:my-16">
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">
         Work Experience
       </h1>
 
-      {/* introduction */}
-      <div>
-        {`I'm a frontend developer, optimist, and community builder. I currently work as the VP of Product at`}
-        <span className="not-prose">
-          <BadgeText>asdf</BadgeText>
-        </span>
-        {`, where I help teach the `}
-        <BadgeText>asdf</BadgeText>
-        {` community, an open-source web framework built with `}
-        <BadgeText>asdf</BadgeText>.
-      </div>
+      {isLoadingCareer || isLoadingProject ? (
+        <CareerSkeleton />
+      ) : (
+        <>
+          {/* introduction */}
+          <Introduction />
 
-      {/* Career List */}
-      {careers.map((career) => (
-        <React.Fragment key={career.properties?.careerId}>
-          <Line />
-          <article id={`career-${career.properties?.careerId}`}>
-            <h2 className="font-medium text-xl mb-1 tracking-tighter">
-              {career.properties?.company}
-            </h2>
-            <p className="text-neutral-600 dark:text-neutral-400 text-sm">
-              {career.properties?.role}, {career.properties?.employmentFrom}-
-              {career.properties?.employmentTo || "Present"}
-            </p>
-            <div className="desc mt-5">
-              <p>
-                I joined <a href="https://vercel.com/home">Vercel</a> early to
-                grow <a href="https://nextjs.org">Next.js</a> and our developer
-                community. I built our Developer Relations team to teach our
-                community about our products.
-              </p>
-            </div>
-            {careerProjects.length > 0 && (
-              <div className="projects mt-5">
-                <h3 className="font-medium text-lg">Projects:</h3>
-                <ul className="list-none">
-                  {careerProjects
-                    .filter((project) => project.careerId[0] === career.id)
-                    .reverse()
-                    .map((project, index) => (
-                      <li key={index} className="mt-2">
-                        <button
-                          onClick={() => toggleProject(project.projectId)}
-                          className="w-full text-left font-semibold text-sm bg-gray-200 dark:bg-gray-400 p-3 rounded-lg dark:text-dark"
-                        >
-                          {index + 1}. {project.title}
-                        </button>
-                        {openProject === project.projectId && (
-                          <div className="project-details mt-2 p-3 bg-gray-100 dark:bg-gray-300 dark:text-dark rounded-lg">
-                            <p>Details for Project {index + 1}</p>
-                          </div>
-                        )}
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            )}
-          </article>
-        </React.Fragment>
-      ))}
+          {/* Career List */}
+          {careers.map((career) => (
+            <React.Fragment key={career.properties?.careerId}>
+              <Line />
+              <article id={`career-${career.properties?.careerId}`}>
+                <h2 className="font-medium text-xl mb-1 tracking-tighter">
+                  {career.properties?.company}
+                </h2>
+                <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+                  {career.properties?.role}, {career.properties?.employmentFrom}
+                  -{career.properties?.employmentTo || "Present"}
+                </p>
+                <div className="desc mt-5">
+                  <p>
+                    I joined <a href="https://vercel.com/home">Vercel</a> early
+                    to grow <a href="https://nextjs.org">Next.js</a> and our
+                    developer community. I built our Developer Relations team to
+                    teach our community about our products.
+                  </p>
+                </div>
+                {careerProjects.length > 0 && (
+                  <div className="projects mt-5">
+                    <h3 className="font-medium text-lg">Projects:</h3>
+                    <ul className="list-none">
+                      {careerProjects
+                        .filter((project) => project.careerId[0] === career.id)
+                        .reverse()
+                        .map((project, index) => (
+                          <li key={index} className="mt-2">
+                            <button
+                              onClick={() => toggleProject(project.projectId)}
+                              className="w-full text-left font-semibold text-sm bg-gray-200 dark:bg-neutral-700 p-3 rounded-lg dark:text-light hover:bg-gray-300 dark:hover:bg-neutral-600"
+                            >
+                              {index + 1}. {project.title}
+                            </button>
+                            {openProject === project.projectId && (
+                              <div
+                                className="project-details mt-2 p-3 bg-gray-100 dark:bg-neutral-800 dark:text-gray-100 rounded-lg 
+                          transition-all duration-300 ease-in-out"
+                              >
+                                <p>Details for Project {index + 1}</p>
+                              </div>
+                            )}
+                          </li>
+                        ))}
+                    </ul>
+                  </div>
+                )}
+              </article>
+            </React.Fragment>
+          ))}
+        </>
+      )}
     </section>
   );
 }
---
+++ src/components/career/Introduction.tsx
@@ -0,0 +1,16 @@
+import BadgeText from "@/components/BadgeText";
+
+export default function Introduction() {
+  return (
+    <div>
+      {`I'm a frontend developer, optimist, and community builder. I currently work as the VP of Product at`}
+      <span className="not-prose">
+        <BadgeText>asdf</BadgeText>
+      </span>
+      {`, where I help teach the `}
+      <BadgeText>asdf</BadgeText>
+      {` community, an open-source web framework built with `}
+      <BadgeText>asdf</BadgeText>.
+    </div>
+  );
+}
