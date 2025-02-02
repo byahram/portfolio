1b3a230d64592740be7e2a1f9b6fc2721c4b7415
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-02-02 22:49:26 +0900

    fix: env 파일 변수명 수정으로 빌드 에러 해결

--- src/app/api/test-axios/route.ts
+++ src/app/api/test-axios/route.ts
@@ -1,16 +1,16 @@
 import notionAxios from "@/lib/axios";
 import { NextResponse } from "next/server";
 
-const NOTION_EXPERIENCE_DB_ID = process.env.NOTION_EXPERIENCE_DB_ID as string;
-if (!NOTION_EXPERIENCE_DB_ID) {
-  throw new Error("Missing NOTION_EXPERIENCE_DB_ID in environment variables.");
+const NOTION_CAREER_DB_ID = process.env.NOTION_CAREER_DB_ID as string;
+if (!NOTION_CAREER_DB_ID) {
+  throw new Error("Missing NOTION_CAREER_DB_ID in environment variables.");
 }
 
 export async function GET() {
   try {
     // notionAxios
     const response = await notionAxios.post(
-      `/databases/${NOTION_EXPERIENCE_DB_ID}/query`,
+      `/databases/${NOTION_CAREER_DB_ID}/query`,
       {}
     );
     // console.log("[notionAxios] response.data.results = ", response.data.results);
--- src/app/api/test-notionhq/route.ts
+++ src/app/api/test-notionhq/route.ts
@@ -1,16 +1,16 @@
 import notionClient from "@/lib/notionhq";
 import { NextResponse } from "next/server";
 
-const NOTION_EXPERIENCE_DB_ID = process.env.NOTION_EXPERIENCE_DB_ID as string;
-if (!NOTION_EXPERIENCE_DB_ID) {
-  throw new Error("Missing NOTION_EXPERIENCE_DB_ID in environment variables.");
+const NOTION_CAREER_DB_ID = process.env.NOTION_CAREER_DB_ID as string;
+if (!NOTION_CAREER_DB_ID) {
+  throw new Error("Missing NOTION_CAREER_DB_ID in environment variables.");
 }
 
 export async function GET() {
   try {
     // notionClient
     const response = await notionClient.databases.query({
-      database_id: NOTION_EXPERIENCE_DB_ID,
+      database_id: NOTION_CAREER_DB_ID,
     });
     // console.log("[notionClient] response.results = ", response.results);
     return NextResponse.json(response.results);
