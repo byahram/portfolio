41e3cdc8e84a9b629a0ab3466cba59a486b11ca0
Ahram Kim <45866137+byahram@users.noreply.github.com>
2025-02-09 18:35:20 +0900

    chore: /images/home 폴더로 이동

--- src/store/store.ts
+++ src/store/store.ts
@@ -9,12 +9,12 @@ export const navList = {
 
 // [Home] Gallery List
 export const galleryList = [
-  { id: 1, src: "/images/mongol.jpg", alt: "몽골" },
-  { id: 2, src: "/images/spring.jpg", alt: "영종도" },
-  { id: 3, src: "/images/hongkong2.jpg", alt: "홍콩 " },
-  { id: 4, src: "/images/mongol2.jpg", alt: "몽골" },
-  { id: 5, src: "/images/jeju.jpg", alt: "한라산" },
-  { id: 6, src: "/images/lv.jpg", alt: "라스베가스 그랜드캐년" },
+  { id: 1, src: "/images/home/mongol.jpg", alt: "몽골" },
+  { id: 2, src: "/images/home/spring.jpg", alt: "영종도" },
+  { id: 3, src: "/images/home/hongkong2.jpg", alt: "홍콩 " },
+  { id: 4, src: "/images/home/mongol2.jpg", alt: "몽골" },
+  { id: 5, src: "/images/home/jeju.jpg", alt: "한라산" },
+  { id: 6, src: "/images/home/lv.jpg", alt: "라스베가스 그랜드캐년" },
 ];
 
 // [About] Profile
